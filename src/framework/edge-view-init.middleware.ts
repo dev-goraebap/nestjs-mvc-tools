0842d5223ff18c6d5efa15c1bf1ce3bdf4e4f17b
dev-goraebap <dev.goraebap@gmail.com>
2025-07-06 15:32:56 +0900

    refactor: 명명 변경

--- src/framework/edge-view-init.middleware.ts
+++ src/framework/edge-view-init.middleware.ts
@@ -8,18 +8,17 @@ import {
 import { BaseLogger } from "../shared/base-logger";
 
 @Injectable()
-export class EdgeInitMiddleware extends BaseLogger implements NestMiddleware {
+export class EdgeViewInitMiddleware extends BaseLogger implements NestMiddleware {
   constructor(
     private readonly edgeView: EdgeView,
     @Inject(NEST_MVC_CORE_OPTIONS)
     private readonly options: NestMvcCoreOptions
   ) {
-    super(EdgeInitMiddleware.name, options);
-    this.debug("init EdgeJsMiddleware");
+    super(EdgeViewInitMiddleware.name, options);
+    this.debug("init EdgeViewInitMiddleware");
   }
 
   use(req: Request, res: Response, next: (error?: any) => void) {
-    // EdgeView 인스턴스를 요청 객체에 주입합니다.
     req.view = this.edgeView;
     next();
   }
