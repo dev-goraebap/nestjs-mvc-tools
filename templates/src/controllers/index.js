b6d94fac60d05da2dcf4053162118f33b3aedcd6
dev-goraebap <dev.goraebap@gmail.com>
2025-07-06 17:57:03 +0900

    feat: HelloWorldController 등록 및 템플릿 수정

--- templates/src/controllers/index.js
+++ templates/src/controllers/index.js
@@ -1,3 +1,4 @@
+import { application } from "./application";
 import { HelloWorldController } from "./hello_world_controller";
 
-application.register('modal-01', HelloWorldController);
+application.register("hello", HelloWorldController);
