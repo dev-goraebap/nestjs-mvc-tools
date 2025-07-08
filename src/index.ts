f9c5fd793e3544a0d2b46e97a065cd7e75b048c3
dev-goraebap <dev.goraebap@gmail.com>
2025-07-08 17:28:33 +0900

    feat: index.ts에서 Edge 관련 모듈을 공유 모듈로 변경하여 내보내기 추가

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export * from "./app/edge.view";
-export * from "./framework/decorators";
-export * from "./framework/exceptions";
-export * from "./framework/csrf.guard";
-export * from "./interfaces/nest-mvc-core-options";
 export * from "./nest-mvc-core.module";
+export * from './shared/decorators';
+export * from './shared/exceptions';
+export * from './shared/flash';
+export * from './shared/interfaces';
+
