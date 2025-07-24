4330a69cca39ec2da191481a50bc2ca240ab306e
dev-goraebap <dev.goraebap@gmail.com>
2025-07-25 03:35:40 +0900

    fix: InternalServerErrorException 메시지를 수정하고 주석 정리

--- tests/manual-test-app/src/controllers/page-exception-test.controller.ts
+++ tests/manual-test-app/src/controllers/page-exception-test.controller.ts
@@ -24,15 +24,12 @@ export class PageExceptionTestController {
    */
   @Get("02")
   test02(@Req() req: NestMvcReq) {
-    throw new InternalServerErrorException("뭔가 이상해요.");
+    throw new InternalServerErrorException("무언가 잘못되었어요.");
   }
 
   /**
    * @test 존재하지 않는 템플릿 엔진 랜더링
    * http://localhost:3000/page-exception-test/03
-   *
-   * 이 경우 500 에러가 발생. HttpException이 아닌 Error. 에러 핸들링을 하려고 하기보단 올바른 경로 입력하도록 하기
-   * 이 외에 템플릿에서 잘못된 데이터 사용등 나는 에러는 사용에 주의. 어떤식으로 라이브러리에서 해결하면 되는지 고민중
    */
   @Get("03")
   test03(@Req() req: NestMvcReq) {
