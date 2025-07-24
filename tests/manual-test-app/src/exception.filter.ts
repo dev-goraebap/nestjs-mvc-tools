b19ed76f12d107232106d25096a32deb79897288
dev-goraebap <dev.goraebap@gmail.com>
2025-07-25 03:35:32 +0900

    refactor: AppExceptionFilter에서 HttpException 처리 방식을 개선하고 코드 정리

--- tests/manual-test-app/src/exception.filter.ts
+++ tests/manual-test-app/src/exception.filter.ts
@@ -4,39 +4,45 @@ import {
   ExceptionFilter,
   HttpException,
   Logger,
-} from '@nestjs/common';
-import { Response } from 'express';
-import { NestMvcBaseExceptionHandler, NestMvcReq } from 'nestjs-mvc-tools';
+} from "@nestjs/common";
+import { Response } from "express";
+import { NestMvcBaseExceptionHandler, NestMvcReq } from "nestjs-mvc-tools";
 
-@Catch(HttpException)
+@Catch()
 export class AppExceptionFilter
   extends NestMvcBaseExceptionHandler
   implements ExceptionFilter
 {
   private readonly logger = new Logger(AppExceptionFilter.name);
 
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: Error | HttpException, host: ArgumentsHost) {
     const req: NestMvcReq = host.switchToHttp().getRequest();
     const res: Response = host.switchToHttp().getResponse();
 
-    this.logger.warn(exception.getStatus());
     this.logger.warn(exception.message);
+
+    // ---------------------------------------------------------
+    // api가 아닌 모든 경로는 페이지 관련 예외 처리
+    // ---------------------------------------------------------
+
+    if (!req.url.startsWith("/api")) {
+      return this.handleMvcException(exception, req, res);
+    }
 
     // ---------------------------------------------------------
     // API 예외처리
     // ---------------------------------------------------------
 
-    if (req.url.startsWith('/api')) {
+    if (exception instanceof HttpException) {
       return res.json({
         status: exception.getStatus(),
         message: exception.message,
       });
+    } else {
+      return res.json({
+        status: 500,
+        message: exception.message,
+      });
     }
-
-    // ---------------------------------------------------------
-    // 페이지 예외처리
-    // ---------------------------------------------------------
-
-    return this.handleMvcException(exception, req, res);
   }
 }
